8ac5582c89d49c22d9881696b5ff2130ab2a27eb
elementkz <adamroitub7@gmail.com>
2023-03-01 18:35:59 +0200

    placed some super-basic code

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+struct CeasarChipher;
+impl CeasarChipher {
+    fn encrypt(value: &str, shift: u8, shift_right: bool) -> &str{
+        if shift == 0 { return value; }
+
+
+
+        
+    }
+}
+
+fn main() {
+    
+}
