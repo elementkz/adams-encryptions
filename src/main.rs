709d9567bbdc86ad2c425d4a4746a8edf1564f24
elementkz <adamroitub7@gmail.com>
2023-03-02 16:07:50 +0200

    added basic implementation of encode/decode to Ceasar Cipher

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,52 @@
+use std::string::FromUtf8Error;
+
+
+/*
+0-25 -> a-z
+26-51 -> A-Z
+52 -> whitespace
+ */
+
 #[derive(Debug)]
 struct CeasarChipher;
 impl CeasarChipher {
-    fn encrypt(value: &str, shift: u8, shift_right: bool) -> &str{
-        if shift == 0 { return value; }
+    fn return_value_in_range(ch: char) {
+        todo!();
+    }
+    fn encode(value: String, shift: u8) -> Result<String, FromUtf8Error> {
+        if shift == 0 {
+            return Ok(value);
+        }
 
+        let mut bytes: Vec<u8> = value.as_bytes().to_vec();
 
+        for i in &mut bytes {
+            *i += shift;
+        }
 
-        
+        return String::from_utf8(bytes);
+    }
+
+    fn decode(value: String, shift: u8) -> Result<String, FromUtf8Error> {
+        if shift == 0 {
+            return Ok(value);
+        }
+
+        let mut bytes: Vec<u8> = value.as_bytes().to_vec();
+
+        for i in &mut bytes {
+            *i -= shift;
+        }
+
+        return String::from_utf8(bytes);
     }
 }
 
 fn main() {
-    
+    let x = String::from("Hello There");
+
+    let x = CeasarChipher::encode(x, 8).unwrap();
+    println!("{}", x);
+    let x = CeasarChipher::decode(x, 8).unwrap();
+    println!("{}", x);
 }
